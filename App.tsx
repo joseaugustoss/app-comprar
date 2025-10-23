503c7dc6cabe2b8a08efb71564ac24944c8ff74c
José Augusto <joseaugusto0408@gmail.com>
2025-10-22 22:29:37 -0300

    icons components

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Home } from "./src/app/Home";
+
+export default function App() {
+  return <Home />;
+}
---
+++ src/app/Home/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+  logo: {
+    height: 34,
+    width: 134,
+  },
+});
