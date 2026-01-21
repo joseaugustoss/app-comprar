a704ac1e64ccabe1c25d974ba2c8fb52b370361c
José Augusto <joseaugusto0408@gmail.com>
2026-01-21 11:55:57 -0300

    componente button

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#2c46b1",
+    height: 48,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 14,
+    fontWeight: 600,
+  },
+});
