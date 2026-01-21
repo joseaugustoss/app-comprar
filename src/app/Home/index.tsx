63a46573b546d0f7711d93caafc3cf04c3dff39c
José Augusto <joseaugusto0408@gmail.com>
2026-01-21 13:23:37 -0300

    propriedades no componentes

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -7,7 +7,7 @@ export function Home() {
     <>
       <View style={styles.container}>
         <Image style={styles.logo} source={require("@/assets/logo.png")} />
-        <Button />
+        <Button title="Entrar" />
       </View>
     </>
   );
