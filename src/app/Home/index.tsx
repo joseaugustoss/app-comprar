1b233f584a81f459e7838d439a852513e3be982d
José Augusto <joseaugusto0408@gmail.com>
2026-01-21 14:20:08 -0300

    componente input

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,12 +1,14 @@
 import { Image, Text, View } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 
 export function Home() {
   return (
     <>
       <View style={styles.container}>
         <Image style={styles.logo} source={require("@/assets/logo.png")} />
+        <Input placeholder="O que você precisa comprar? " />
         <Button title="Entrar" />
       </View>
     </>
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    height: 48,
+    borderRadius: 8,
+    borderWidth: 1,
+    paddingHorizontal: 16,
+    fontSize: 14,
+    width: "100%",
+    borderColor: "#c3c5cb",
+  },
+});
