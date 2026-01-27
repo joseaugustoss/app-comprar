e28918ed6358a655d7785e9ddd79342b30b2f15a
José Augusto <joseaugusto0408@gmail.com>
2026-01-27 19:01:15 -0300

    FlatList

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, View, TouchableOpacity, ScrollView } from "react-native";
+import { Image, Text, View, TouchableOpacity, FlatList } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
@@ -7,6 +7,7 @@ import { FilterStatus } from "@/types/FilterStatus";
 import { Item } from "@/components/Item";
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
+const ITENS = Array.from({ length: 100 }).map((_, index) => String(index));
 export function Home() {
   return (
     <View style={styles.container}>
@@ -24,11 +25,16 @@ export function Home() {
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
-        <ScrollView>
-          {Array.from({ length: 100 }).map((value, index) => (
+
+        <FlatList
+          data={ITENS}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
             <Item
-              key={index}
-              data={{ status: FilterStatus.PENDING, description: "Café" }}
+              data={{
+                status: FilterStatus.PENDING,
+                description: item,
+              }}
               onRemove={() => {
                 console.log("remove");
               }}
@@ -36,8 +42,8 @@ export function Home() {
                 console.log("mudar o status");
               }}
             />
-          ))}
-        </ScrollView>
+          )}
+        />
       </View>
     </View>
   );
